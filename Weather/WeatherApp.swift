3f6c5988eb0d35cfb9bef583f3e0060a35a28233
Marcelo Amoretti <marceloamoretti@gmail.com>
2023-09-01 11:25:02 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Marcelo on 2023-09-01.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
